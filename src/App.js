197b39c6dffb281b5ea96f08591257961f286a33
Jon Hobbs <jon.hobbs@minnykid.com>
2020-08-14 14:54:39 -0500

    app js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react'
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom'
 import { createBrowserHistory } from "history"
 import { connect } from 'react-redux'
-import 'primereact/resources/themes/rhea/theme.css'
+
+import './Assets/wwpm-theme/theme.css'
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
 
 import AuthPage from './Pages/Auth'
 import ProjectsPage from './Pages/Projects'
+import ProjectDetailsPage from './Pages/ProjectDetails'
 import ProfilePage from './Pages/Profile'
 import './App.css'
 
@@ -15,17 +17,18 @@ const history = createBrowserHistory();
 class App extends Component {
   render() {
     return (
-      <BrowserRouter history={history}>
-        <React.Fragment>
-            <Switch>
-              {this.props.token && <Redirect from="/auth" to="/projects" exact />}
-              {!this.props.token && <Route path="/auth" component={AuthPage} />}
-              {!this.props.token && <Redirect to="/auth" exact />}
-              {<Route path="/projects" component={ProjectsPage} />}
-              {<Route path="/profile" component={ProfilePage} />}
-            </Switch>
-        </React.Fragment>
-      </BrowserRouter>
+          <BrowserRouter history={history}>
+            <React.Fragment>
+                <Switch>
+                  {this.props.token && <Redirect from="/auth" to="/projects" exact />}
+                  {!this.props.token && <Route path="/auth" component={AuthPage} />}
+                  {!this.props.token && <Redirect to="/auth" exact />}
+                  {<Route path="/projects" component={ProjectsPage} />}
+                  {<Route path="/project-details/:id" component={ProjectDetailsPage} />}
+                  {<Route path="/profile" component={ProfilePage} />}
+                </Switch>
+            </React.Fragment>
+          </BrowserRouter>
     );
   }
 }
