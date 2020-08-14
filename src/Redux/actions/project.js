24a2d3b6c847d42eb30c22f102639987f90dda12
Jon Hobbs <jon.hobbs@minnykid.com>
2020-08-14 14:28:03 -0500

    redux action

--- src/Redux/actions/project.js
+++ src/Redux/actions/project.js
@@ -1,6 +1,8 @@
 import { REQUEST_PROJECTS,
+  REQUEST_PROJECT,
   UPDATE_PROJECT,
   PROJECTS_RECEIVED,
+  PROJECT_RECEIVED,
   PROJECT_UPDATED,
   CREATE_PROJECT,
   PROJECT_CREATED,
@@ -22,6 +24,19 @@ export const projectsReceived = (projects) => {
   return {
     type: PROJECTS_RECEIVED,
     projects: projects
+  }
+}
+
+export const requestProject = () => {
+  return {
+    type: REQUEST_PROJECT
+  }
+}
+
+export const projectReceived = (project) => {
+  return {
+    type: PROJECT_RECEIVED,
+    project: project
   }
 }
 
@@ -67,10 +82,16 @@ export const projectDeleted = (deletedProject) => {
 
 export const fetchProjects = () => async (dispatch, getState) => {
   const state = getState()
-  if (state.projects.projects && state.projects.projects.length > 0) return
   dispatch(requestProjects())
   const projects = await api.getProjects(state.auth.token)
   dispatch(projectsReceived(projects))
+}
+
+export const fetchProjectDetails = id => async (dispatch, getState) => {
+  const state = getState()
+  dispatch(requestProject())
+  const project = await api.getProjectDetails(id, state.auth.token)
+  dispatch(projectReceived(project))
 }
 
 export const saveProject = project => async (dispatch, getState) => {
@@ -87,9 +108,9 @@ export const createProject = project => async (dispatch, getState) => {
   dispatch(projectCreated(newProject))
 }
 
-export const deleteProject = project => async (dispatch, getState) => {
+export const deleteProject = id => async (dispatch, getState) => {
   const state = getState()
   dispatch(deletingProject())
-  const deletedProject = await api.deleteProject(project, state.auth.token)
+  const deletedProject = await api.deleteProject(id, state.auth.token)
   dispatch(projectDeleted(deletedProject))
 }
