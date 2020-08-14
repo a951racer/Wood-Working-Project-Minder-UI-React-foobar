1f3a7572a427d8de7db04f05a39b4e3bbf108160
Jon Hobbs <jon.hobbs@minnykid.com>
2020-08-14 14:20:23 -0500

    Boards.js

---
+++ src/Components/Boards.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+import { DataTable } from 'primereact/datatable'
+import { Column } from 'primereact/column'
+import { Accordion, AccordionTab } from 'primereact/accordion';
+import { Growl } from 'primereact/growl'
+
+import BoardTable from './BoardTable'
+
+class Boards extends Component {
+  constructor(props) {
+    super(props)
+    console.log('props: ', props)
+    this.state = {
+      editingBoard: null,
+      //sheetBoards: this.props.boards.filter((board) => board.material === 'Sheet'),
+      //hardwoodBoards: this.props.boards.filter((board) => board.material === 'Hardwood'),
+      //dimensionalBoards: this.props.boards.filter((board) => board.material === 'Dimensional'),
+    }
+  }
+
+
+  render() {
+    const sheetBoards = this.props.boards ? this.props.boards.filter((board) => board.material === 'Sheet') : null
+    const hardwoodBoards = this.props.boards ? this.props.boards.filter((board) => board.material === 'Hardwood') : null
+    const dimensionalBoards = this.props.boards ? this.props.boards.filter((board) => board.material === 'Dimensional') : null
+    return (
+        <>
+        <Accordion multiple={true}>
+          <AccordionTab header="Sheet Goods">
+            <BoardTable boards={sheetBoards} />
+          </AccordionTab>
+          <AccordionTab header="Hardwood">
+            <BoardTable boards={hardwoodBoards} />
+          </AccordionTab>
+          <AccordionTab header="Dimensional">
+            <BoardTable boards={dimensionalBoards} />
+          </AccordionTab>
+        </Accordion>
+
+        <Growl ref={(el) => this.growl = el} />
+
+        <br />
+        </>
+    )
+  }
+}
+
+export default Boards
