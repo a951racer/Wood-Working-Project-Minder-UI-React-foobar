596623e3131e00b6c6e7b5845c10716dd7e07094
Jon Hobbs <jon.hobbs@minnykid.com>
2020-08-14 14:46:59 -0500

    redux auth reducer

--- src/Redux/reducers/auth.js
+++ src/Redux/reducers/auth.js
@@ -1,47 +1,47 @@
 import { REQUEST_LOGIN,
-         LOGIN_SUCCESS,
-         LOGIN_FAIL,
-        LOGOUT } from "../actionTypes/auth";
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+ LOGOUT } from "../actionTypes/auth";
 
 const initialState = {
-  userStatus: 'loggedOut',
-  userId: null,
-  token: null,
-  tokenExpiration: null
+userStatus: 'loggedOut',
+userId: null,
+token: null,
+tokenExpiration: null
 };
 
 const auth = (state = initialState, action) => {
-  switch (action.type) {
-    case REQUEST_LOGIN: {
-      return { userStatus: 'loggingIn' }
-    }
-    case LOGIN_SUCCESS: {
-      return {
-        userStatus: 'loggedIn',
-        userId: action.userId,
-        token: action.token,
-        tokenExpiration: action.tokenExpiration
-      } 
-    }
-    case LOGIN_FAIL: {
-      return {
-        userStatus: 'loggedOut',
-        userId: null,
-        token: null,
-        tokenExpiration: null
-      }
-    }
-    case LOGOUT: {
-      return {
-        userStatus: 'loggedOut',
-        userId: null,
-        token: null,
-        tokenExpiration: null
-      }
-    }
-    default:
-      return state;
-  }
+switch (action.type) {
+case REQUEST_LOGIN: {
+return { userStatus: 'loggingIn' }
+}
+case LOGIN_SUCCESS: {
+return {
+ userStatus: 'loggedIn',
+ userId: action.userId,
+ token: action.token,
+ tokenExpiration: action.tokenExpiration
+} 
+}
+case LOGIN_FAIL: {
+return {
+ userStatus: 'loggedOut',
+ userId: null,
+ token: null,
+ tokenExpiration: null
+}
+}
+case LOGOUT: {
+return {
+ userStatus: 'loggedOut',
+ userId: null,
+ token: null,
+ tokenExpiration: null
+}
+}
+default:
+return state;
+}
 }
 
 export default auth
